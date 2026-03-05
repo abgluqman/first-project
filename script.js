4430ac492e5217c6060fe10a10a69349d841cb64
abgluqman <abangluqmantahir@gmail.com>
2026-03-05 15:17:55 +0800

    2nd draft message

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "please...",
+    "Really really sure?",
+    "Really really really sure?",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
